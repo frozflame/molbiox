91c9eb1825ce693e40a39a2ab96e896cb2db235b
frozflame <lendoli@163.com>
2014-10-11 16:51:38 +0800

    premature

---
+++ molbiox/__init__.py
@@ -0,0 +1,4 @@
+
+from molbiox.local.conf 
+
+
---
+++ molbiox/local/getconf.py
@@ -0,0 +1,21 @@
+
+
+from os.path import abspath, split
+
+def updir(path):
+    """
+    Go up until root dir (/), resembles
+    
+        while PWD != /
+            pwd; cd ..;
+        pwd
+    """
+    if not 
+
+    while True:
+        
+
+def getdir(path='.'):
+    path = abspath('.')
+    
+    
