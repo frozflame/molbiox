cae55f8c7e78f1e40f144e135352d5ece8095d0d
Hailong <lendoli@163.com>
2015-07-05 20:15:17 +0800

    add file and content: io.lenfile

---
+++ molbiox/io/lenfile.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+from __future__ import unicode_literals
+
+__author__ = 'Hailong'
+
+
+def read(filename, verbose=False):
+    """
+    Parse file format like `wc` or `fastalength` output
+
+    Say we have
+
+        264531 2045177 65877908 google.chrome.dmg
+        181167 1408149 48452451 picasa.mac.39.dmg
+        ...
+
+    Then we will get
+
+        {
+            'google.chrome.dmg': 264531,
+            'picasa.mac.39.dmg': 181167,
+        }
+
+    Or if `tupl` is True
+
+        {
+            'google.chrome.dmg': (264531, 2045177, 65877908),
+            'picasa.mac.39.dmg': (181167, 1408149, 48452451),
+        }
+    """
+    resdict = dict()
+    for line in open(filename):
+        line = line.strip()
+
+        # skip empty line or comment
+        if not line or line.startswith('#'):
+            continue
+
+        itemlist = line.split()
+        key = itemlist[-1]
+
+        if verbose:
+            val = tuple(int(x) for x in itemlist[:-1])
+        else:
+            val = int(itemlist[0])
+        resdict[key] = val
+    return resdict
+
+
