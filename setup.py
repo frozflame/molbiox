c94299923e320b086d854875c6fac25b8ff82cf1
Hailong <lendoli@163.com>
2015-07-05 20:14:39 +0800

    setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
-__author__ = 'Hailong'
+from setuptools import setup, find_packages
+
+
+def readfile(filename):
+    with open(filename) as f:
+        return f.read()
+
+
+setup(
+    name="molbiox",
+    version="0.0.1",
+    packages=find_packages(),
+    zip_safe=False,
+    namespace_packages=["molbiox"],
+    install_requires=readfile("requirements.txt"),
+
+    # ensure copy static file to runtime directory
+    include_package_data=True,
+)
+
