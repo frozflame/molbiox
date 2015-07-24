f0d7e827d1510537d554d33d83f6235802960b13
Hailong <lendoli@163.com>
2015-07-25 00:10:37 +0800

    mbx.knowledge.lipopolysaccharide: gene names like wzx ...

---
+++ molbiox/knowledge/lipopolysaccharide.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+from __future__ import unicode_literals
+
+gene_protein_map = {
+    'wzx':  'O-antigen flippase Wzx',
+    'wzy':  'O-antigen polymerase Wzy',
+    'wzz':  'Chain length determinant protein Wzz',
+
+    'wza':  'Polysaccharide export protein Wza',
+    'wzb':  'Low molecular weight protein-tyrosine-phosphatase Wzb',
+    'wzc':  'Tyrosine-protein kinase Wzc',
+    'wzd':  'Capsular polysaccharide biosynthesis protein Wzd',
+
+    'wecA': 'Undecaprenyl-phosphate alpha-N-acetylglucosaminyl 1-phosphate transferase WecA',
+
+    'wbjC': 'UDP-2-acetamido-2,6-beta-L-arabino-hexul-4-ose reductase WbjC',
+
+    'wbbJ': 'lipopolysaccharide biosynthesis O-acetyl transferase WbbJ',
+
+    'galE': 'UDP-glucose 4-epimerase GalE',
+    'pseG': 'Pseudaminic acid biosynthesis-associated protein PseG',
+    'amsE': 'Amylovoran biosynthesis protein AmsE',
+    'exoZ': 'Exopolysaccharide production protein ExoZ',
+
+    'manA': 'Mannose-6-phosphate isomerase manA',
+    'manB': 'Phosphomannomutase manB',
+    'manC': 'Mannose-1-phosphate guanylyltransferase manC',
+    'manD': 'D-mannonate dehydratase manD',
+
+    'cpsB': 'Mannose-1-phosphate guanyltransferase CpsB',
+
+    'rmlA': 'Glucose-1-phosphate thymidylyltransferase RmlA',
+    'rmlB': 'dTDP-glucose 4,6-dehydratase RmlB',
+    'rmlC': 'dTDP-4-dehydrorhamnose 3,5-epimerase RmlC',
+    'rmlD': 'dTDP-6-deoxy-L-lyxo-4-hexulose reductase RmlD',
+
+    'rfbB': 'O-antigen export system ATP-binding protein RfbB',
+    'rjg':  'Putative uncharacterized protein Rjg',
+
+
+    'gmd':  'GDP-mannose 4,6-dehydratase Gmd',
+    'capD': 'Capsule biosynthesis protein CapD',
+
+    'yhhI': 'H repeat-associated protein YhhI',
+
+    'gmhD': 'ADP-L-glycero-D-manno-heptose-6-epimerase HldD/GmhD',
+    'hldD': 'ADP-L-glycero-D-manno-heptose-6-epimerase HldD/GmhD',
+}
