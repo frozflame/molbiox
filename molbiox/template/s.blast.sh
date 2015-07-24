72589cd1104dea821c6d77d4d5f4db41383b7b7e
Hailong <lendoli@163.com>
2015-07-24 19:10:57 +0800

    template: s.blast.sh, add more options commented for easy and fast editing

--- molbiox/template/s.blast.sh
+++ molbiox/template/s.blast.sh
@@ -2,7 +2,18 @@
 
 NTHREADS="6"
 BLASTEXE="blastp"
-DATABASE="/home/hailong/Database/ncbi/nr"
+
+# DBDIR="."
+# DBDIR="/mnt/fx6300/hailong/Public/dbxbio"
+DBDIR="$HOME/Public/dbxbio"
+
+DATABASE="ncbi/nr"
+# DATABASE="uniprot/uniprot_sprot.named.fasta"
+# DATABASE="uniprot/wzx.named.fasta"
+# DATABASE="uniprot/wzy.named.fasta"
+# DATABASE="uniprot/wzxy.named.fasta"
+# DATABASE="uniprot/oprm.named.fasta"
+# DATABASE="uniprot/oprm.named.fasta"
 
 EVALUE="1e-5"
 
@@ -10,7 +21,7 @@ for QUERY in $@; do
 
     OUTNAME=${QUERY}.fmt11.${BLASTEXE}
 
-    ${BLASTEXE}  -query ${QUERY}  -db ${DATABASE}  -outfmt 11 \
+    ${BLASTEXE}  -query ${QUERY}  -db ${DBDIR}/${DATABASE}  -outfmt 11 \
                  -out ${OUTNAME}  -num_threads ${NTHREADS}  -evalue ${EVALUE}
 
     # blast_formatter -archive ${OUTNAME} -outfmt 0 > ${QUERY}.fmt0.${BLASTEXE}
--- molbiox/template/s.glimmer.predict.parse.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-# coding: utf-8
-
-from __future__ import unicode_literals
-
-__author__ = 'Hailong'
