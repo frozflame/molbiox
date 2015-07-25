4167d40786523488afdf816af6cbca09cfa2cae8
Hailong <lendoli@163.com>
2015-07-25 09:26:34 +0800

    algor.interval.overlap: size of overlap of 2 intervals

---
+++ molbiox/algor/interval.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+import math
+
+
+def overlap(a1, a2, b1, b2):
+    """
+    Calculate overlap size of intervals (a1, a2) and (b1, b2)
+    """
+    alen = abs(a1-a2)
+    blen = abs(b1-b2)
+    wide = max(a1, a2, b1, b2) - min(a1, a2, b1, b2)
+    return alen + blen - wide
+
+
+def group_by_overlap(intervals):
+    pass
+
+
