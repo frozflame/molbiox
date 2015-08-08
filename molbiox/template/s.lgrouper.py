ddf2d5d52ca92f9edb14c87103d5a217f7e159cf
Hailong <lendoli@163.com>
2015-08-08 15:30:59 +0800

    io -> iofmt, knowledge -> info

--- molbiox/template/s.lgrouper.py
+++ molbiox/template/s.lgrouper.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 from __future__ import unicode_literals, print_function
-from molbiox.io.tabfile import LGrouper
+from molbiox.iofmt.tabfile import LGrouper
 
 import os
 import sys
