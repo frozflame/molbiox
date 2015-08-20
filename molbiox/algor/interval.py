90f49d62f58e9cea5f6b32de24e6b968f44a59ed
Hailong <lendoli@163.com>
2015-08-20 10:56:07 +0800

    overlap -> overlap_size

--- molbiox/algor/interval.py
+++ molbiox/algor/interval.py
@@ -4,7 +4,7 @@
 import math
 
 
-def overlap(a1, a2, b1, b2):
+def overlap_size(a1, a2, b1, b2):
     """
     Calculate overlap size of intervals (a1, a2) and (b1, b2)
     """
@@ -12,6 +12,10 @@ def overlap(a1, a2, b1, b2):
     blen = abs(b1-b2)
     wide = max(a1, a2, b1, b2) - min(a1, a2, b1, b2)
     return alen + blen - wide
+
+
+def overlap(a1, a2, b1, b2):
+    pass
 
 
 def group_by_overlap(intervals):
