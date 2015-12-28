993a9fa57aff21c4c1184aeb05d0e82038f069ad
Hailong <lendoli@163.com>
2015-12-28 16:47:27 +0800

    v0.0.3

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def readfile(filename):
 
 setup(
     name="molbiox",
-    version="0.0.2",
+    version="0.0.3",
     description="utilities for bioinformatics",
     url="https://github.com/frozflame/molbiox",
     author='frozflame',
