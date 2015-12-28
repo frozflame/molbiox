d4aad5db8313c5d9b1682403db4694b351fc2b33
Hailong <lendoli@163.com>
2015-12-28 14:37:35 +0800

    v0.0.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def readfile(filename):
 
 setup(
     name="molbiox",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
     zip_safe=False,
     namespace_packages=["molbiox"],
