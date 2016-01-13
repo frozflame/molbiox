6ddc5ae62a655e7bf7ce8956e8462844b138de94
Hailong <lendoli@163.com>
2016-01-13 20:39:37 +0800

    mn: Aggregator -- in progress

--- molbiox/execute/relational.py
+++ molbiox/execute/relational.py
@@ -3,35 +3,41 @@
 
 from __future__ import unicode_literals, print_function
 import sys
-import sys
 import itertools
 from molbiox.frame.command import Command
-from molbiox.io import fasta, blast
+from molbiox.io import blast, tabular
 
+"""
+If your results come from more than 2 columns, use a SQL database instead.
+"""
 
-class CommandHitAggregate(Command):
-    abbr = 'a'
-    name = 'hit-aggregate'
-    desc = 'aggregate database search results'
+class CommandAggregate(Command):
+    abbr = 'ag'
+    name = 'aggregate'
+    desc = 'apply an aggregation function to a tabular text file'
 
     @classmethod
     def register(cls, subparser):
-        # a dummy register for expansiion
-        subparser = super(CommandHitAggregate, cls).register(subparser)
+        subparser = super(cls, cls).register(subparser)
         subparser.add_argument(
-            # note: nargs=1 shoud NOT be here
             '--subsep', metavar='character',
             help="seperator used on subject names")
         subparser.add_argument(
-            # note: nargs=1 shoud NOT be here
-            '-f', '--format', metavar='string', default='6m',
-            help='currently only blast6mini')
+            '-f', '--function', metavar='string', default='count',
+            choices=['count', 'list', 'set', 'avg', 'var', 'std'],
+            help='name of the aggregation function')
         subparser.add_argument(
-            '-l', '--list', action='store_true',
-            help='list subject names instead of count')
+            '-k', '--key', metavar='integer', default=0,
+            help='group by this column ')
         subparser.add_argument(
-            '-s', '--sort', action='store_true',
-            help='sort output alphabetically')
+            '-v', '--val', metavar='integer', default=0,
+            help='apply aggregation function on this column')
+        subparser.add_argument(
+            '--ksort', metavar='string', choices=['alpha', 'num'],
+            help='sort keys alphabetically or numerically')
+        subparser.add_argument(
+            '--vsort', metavar='string', choices=['alpha', 'num'],
+            help='sort values alphabetically or numerically')
         subparser.add_argument(
             '-m', '--limit', type=int,
             help='set max number of hits listed for each query')
@@ -39,6 +45,17 @@ class CommandHitAggregate(Command):
 
     @classmethod
     def render(cls, args, outfile):
+        recgens = [tabular.read(fn) for fn in args.filenames]
+        records = itertools.chain(*recgens)
+        aggregator = tabular.Aggregator(aggregator)
+
+        if args.function == 'count':
+            # groups = aggregator
+            pass
+
+
+    @classmethod
+    def render_(cls, args, outfile):
         if args.format != '6m':
             sys.exit('currently only blast6mini')
 
@@ -64,3 +81,14 @@ class CommandHitAggregate(Command):
         else:
             for k, v in querydic.items():
                 print(len(v), k, sep='\t', file=outfile)
+
+    @classmethod
+    def get_agg_func(cls, name):
+        """
+        Get a function which returns a dict-like object
+        :param name:
+        :return:
+        """
+        pass
+
+
