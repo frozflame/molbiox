967c2179c584f117c014a49c88dc047d37895372
Hailong <lendoli@163.com>
2016-04-29 01:42:39 +0800

    translate: non-ATGC -> X

--- molbiox/algor/translate.py
+++ molbiox/algor/translate.py
@@ -15,27 +15,33 @@ class CodonTable(object):
         packarr = self._gen_packarr(''.join(condons))
 
         # max(packarr) < 64 (2**6)
-        self.refarr = np.empty(64, dtype='uint8')
+        self.refarr = np.empty(128, dtype='uint8')
 
         # refarr[packed_value] = amino_acid
         self.refarr[packarr] = [ord(dic[c]) for c in condons]
+        self.refarr[64:] = ord('X')
 
     @classmethod
     def _gen_packarr(cls, string):
         """
-        A   01000001
-        C   01000011
-        G   01000111    ->  0000 0010
-        T   01010100
-        :param string: assuming containing only ATCG and len(string) % 3 == 0
+        A   0100 0001
+        C   0100 0011
+        G   0100 0111   ->  0100 0010
+        T   0101 0100   ->  0100 0000
+        :param string: containing only upper-case letters and len(string) % 3 == 0
         :return:
         """
         arr = np.fromstring(string, dtype='uint8')
+        arr[arr == ord('T')] = 0
+        arr[arr == ord('A')] = 1
         arr[arr == ord('G')] = 2
+        arr[arr == ord('C')] = 3
+
+        arr[arr > 3] = int('01010101', base=2)
+
         arr.shape = -1, 3
 
         # mask out unwanted bits
-        arr &= 3
         arr <<= np.array([0, 2, 4], dtype='uint8')
 
         # 50x faster than arr.sum(axis=1)
