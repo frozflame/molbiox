4c547a1b036db33bd1dbcc50fa5754d0400d5303
Hailong <lendoli@163.com>
2016-05-01 20:20:43 +0800

    svg maker

--- molbiox/visual/svg_maker.py
+++ molbiox/visual/svg_maker.py
@@ -2,10 +2,24 @@
 # coding: utf-8
 
 from __future__ import unicode_literals, print_function
-import sys
-import numpy
+
 import six
-from lxml.builder import E, ET
+from lxml.builder import E
+
+
+default_polygon_style = {
+    'stroke-width': 1,
+    'fill-opacity': 0.7,
+    'stroke': 'black',
+    'stroke-opacity': 0.8,
+    'fill': 'red',
+}
+
+
+default_text_style = {
+    'font-size': 9,
+    'font-family': 'Times New Roman',
+}
 
 
 def format_style(style):
@@ -22,19 +36,25 @@ def format_points(points):
     return ' '.join('{},{}'.format(*pair) for pair in points)
 
 
-def make_polygon(points, style):
+def make_polygon(points, style=None):
+    user_style = style or dict()
+    style = dict(default_polygon_style)
+    style.update(user_style)
+    style = format_style(style)
     if not isinstance(points, six.string_types):
         points = format_points(points)
-    if not isinstance(style, six.string_types):
-        style = format_style(style)
     return E.polygon(points=points, style=style)
 
 
-def make_text(content, style, x, y, rx, ry, angle=0):
+def make_text(content, x, y, rx, ry, angle=0, style=None):
+    user_style = style or dict()
+    style = dict(default_polygon_style)
+    style.update(user_style)
     attributes = {
         'x': '{}'.format(x),
         'y': '{}'.format(y),
-        'transform': 'rotate({} {},{})'.format(angle, rx, ry)
+        'transform': 'rotate({} {},{})'.format(angle, rx, ry),
+        'style': format_style(style),
     }
     return E.text(content, **attributes)
 
