ee672dfcb047e8f6f069dbd048e721337abb6b7a
Hailong <lendoli@163.com>
2016-05-01 21:13:26 +0800

    new: visual.vizorf

---
+++ molbiox/visual/vizorf.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+from __future__ import division, unicode_literals, print_function
+
+from molbiox.algor.arrowgen import ArrowGen
+from molbiox.frame import interactive, streaming
+from molbiox.io import tabular
+from molbiox.visual import svg_maker
+
+
+@interactive.castable
+def rescale_tab_vizorf(records, scale, normalize=True):
+    """
+    Cannot handle very large file
+    :param records: an iterable of TabRecord objects
+    :param normalize: boolean. If true, shift to make leftmost point 0
+    :param scale: a number. All head and tail values will be divided by this num
+    :return:
+    """
+    if normalize:
+        records = records if isinstance(records, list) else list(records)
+        minpos = min(r.head for r in records)
+    else:
+        minpos = 0.
+
+    for rec in records:
+        # element = get_defaults(polygon_style=dict(fill=rec['color']))
+        # print(element, file=sys.stderr)
+        head = rec.head
+        tail = rec.tail
+        if rec.strand == '-':
+            rec.head = (tail - minpos) * 1. / scale
+            rec.tail = (head - minpos) * 1. / scale
+        else:
+            rec.head = (head - minpos) * 1. / scale
+            rec.tail = (tail - minpos) * 1. / scale
+        yield rec
+
+
+class TextMaker(object):
+    def __init__(self, height, angle=0, style=None):
+        self.angle = angle
+        self.style = style
+        self.height = height
+
+    def __call__(self, record):
+        c = record.label
+        x = (record.head + record.tail) / 2.
+        y = self.height
+        a = self.angle
+        s = self.style
+        return svg_maker.make_text(c, x=x, y=y, rx=x, ry=y, angle=a, style=s)
+
+
+def render_vizorf(filename, scale, normalize, a, b, h1, h2, style):
+    records = tabular.read_tab_vizorf(filename, castfunc=list)
+    records = rescale_tab_vizorf(records, scale, normalize)
+
+    # print('debug: elements', elements, file=sys.stderr)
+    arrowgen = ArrowGen(a, b, h1, h2)
+    ypos = h2 * 4
+    arrpos = ([r.head, r.tail, ypos] for r in records)
+    arrpgs = arrowgen(arrpos)
+
+    tm = TextMaker(h2, angle=-30, style=style)
+    texts = [tm(r) for r in records]
+    polygons = [svg_maker.make_polygon(a) for a in arrpgs]
+
+    elements = streaming.alternate(texts, polygons)
+    width = max(r.tail for r in records)
+    return svg_maker.render_svg(elements=elements, height=h2*5, width=width)
